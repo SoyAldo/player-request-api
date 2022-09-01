9d855f3740541bb86b1e266ad89c5a937c71037b
SoyAldo <pereyraosvaldo@live.com.ar>
2022-09-01 17:32:16 -0300

    PlayerQuitListener.java updated

--- src/main/java/org/kayteam/playerrequestapi/listeners/PlayerQuitListener.java
+++ src/main/java/org/kayteam/playerrequestapi/listeners/PlayerQuitListener.java
@@ -1,6 +1,31 @@
 package org.kayteam.playerrequestapi.listeners;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.kayteam.playerrequestapi.PlayerRequestManager;
+
+import java.util.UUID;
 
 public class PlayerQuitListener implements Listener {
+
+    private final PlayerRequestManager playerRequestManager;
+
+    public PlayerQuitListener(PlayerRequestManager playerRequestManager) {
+        this.playerRequestManager = playerRequestManager;
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+
+        Player player = event.getPlayer();
+
+        UUID uuid = player.getUniqueId();
+
+        playerRequestManager.removeRequests(uuid);
+
+    }
+
+
 }
